557ddb1e0888ab344da3b410f4703c92128aaf19
Alex <past6867@gmail.com>
2025-01-22 12:53:59 -0500

    Index multiples formas

---
+++ Práctica.py
@@ -0,0 +1,12 @@
+palabra = "ordenador"
+resultado = palabra[4]
+print(resultado)
+
+
+frase = "En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+resultado = frase.index("práctica")
+print(resultado)
+
+frase = "En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+resultado = frase.rindex("práctica")
+print(resultado)
---
+++ index.py
@@ -0,0 +1,5 @@
+mi_texto = "Este es una prueba"
+resultado = mi_texto[-10]
+resultado = mi_texto.index("a")
+resultado = mi_texto.rindex("e")
+print(resultado)
