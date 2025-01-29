70333734d81caaa30fff2d740cf75ac7bac2adbc
Alex <past6867@gmail.com>
2025-01-28 21:54:46 -0500

    Practica de metodos

---
+++ Metodos.py
@@ -0,0 +1,16 @@
+
+
+
+texto = "Este es el texto de Alex"
+result = texto.upper()
+print(result)
+
+result = texto.split("t")
+print(result)
+
+a = 'Aprender'
+b = 'Python'
+c = 'es'
+d = 'genial'
+e = " ". join([a,b,c,d])
+print(e)
---
+++ Práctica-03.py
@@ -0,0 +1,13 @@
+# frase = "Especialmente en las comunicaciones electrónicas, la escritura enteramente en mayúsculas equivale a gritar."
+# resultado = frase.upper()
+# print(resultado)
+
+# lista_palabras = ["La","legibilidad","cuenta."]
+# resultado = " ".join(lista_palabras)
+# print(resultado)
+
+frase = "Si la implementación es difícil de explicar, puede que sea una mala idea."
+resultado = frase.replace("difícil","facil").replace("mala","buena")
+print(resultado)
+
+
