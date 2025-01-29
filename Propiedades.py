72a8eca84f5f0827ed8b7962d7d22f5a110fb717
Alex <past6867@gmail.com>
2025-01-28 22:56:20 -0500

    Propiedades

---
+++ Propiedades.py
@@ -0,0 +1,11 @@
+nombre = "Carina"
+Resultado = nombre.replace("C","K")
+print(Resultado)
+
+frase = """Hola 
+mundo"""
+print(frase)
+
+print("Hola" in frase)
+
+print(len(frase))
---
+++ Práctica-04.py
@@ -0,0 +1,8 @@
+print("Repetición" * 15)
+
+texto = """Tierra mojada
+mis recuerdos de viaje,
+entre las lluvias"""
+print("agua" not in texto)
+
+print(len("electroencefalografista"))
